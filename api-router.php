49bec2e7124ef3aef9864df4e909730d2f0f496c
Victoria Oliveto <mvictoriaoliveto@gmail.com>
2022-11-15 02:11:40 -0300

    f

--- api-router.php
+++ api-router.php
@@ -7,7 +7,7 @@ $router = new Router();
 
 $router->addRoute('zapatilla', 'GET', 'controllerApi', 'getZapatilla');
 $router->addRoute('zapatilla/:ID', 'GET', 'controllerApi', 'getZapatillaId');
-$router->addRoute('zapatilla', 'GET', 'controllerApi', 'homeFilter'); 
+//$router->addRoute('zapatilla', 'GET', 'controllerApi', 'homeFilter'); 
 $router->addRoute('zapatilla', 'POST', 'controllerApi', 'insertZapatilla');
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
